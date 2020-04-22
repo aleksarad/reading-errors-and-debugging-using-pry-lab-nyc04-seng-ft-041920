d0a5530873c2bc0a1ffa2b9e8cb5884cb134ef2e
Alexandra Radevich <alexa.radevich1@gmail.com>
2020-04-22 15:20:57 +0000

    Done.

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -1,9 +1,13 @@
 # don't forget to add: require 'pry'
+require 'pry'
 
 def snake_it_up(string)
   if string[0] == "s"
-    10 * "s" + string
+    s_print = "ssssssssss"
+    s_print + string
   else
     string
   end
 end
+
+snake_it_up("so")
